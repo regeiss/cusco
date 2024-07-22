7c2320f11fa9bc4ee33f2b765140752d2b9c60a4
regeiss <roberto.geiss.nh@gmail.com>
2024-07-22 16:16:11 -0300

    router

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:gtk_flutter/firebase_options.dart';
 import 'package:gtk_flutter/src/app/views/app_view.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: AppView()));
-
 }
+
+final _router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      name: 'home', // Optional, add name to your routes. Allows you navigate by name instead of path
+      path: '/',
+      builder: (context, state) => HomeScreen(),
+    ),
+    GoRoute(
+      name: 'shope',
+      path: '/shope',
+      builder: (context, state) => ShopeScreen(),
+    ),
+  ],
+);
